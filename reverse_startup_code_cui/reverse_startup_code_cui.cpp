7dafc6e53a0baf58f3a9113b5defa0ca0ac001ed
Sima214 <anastasios2014@gmail.com>
2019-04-10 13:11:43 +0300

    Add startup files.

---
+++ reverse_startup_code_cui/reverse_startup_code_cui.cpp
@@ -0,0 +1,13 @@
+/**
+ * reverse_startup_code_cui.cpp :
+ *	This file contains the 'main' function.
+ *	Program execution begins and ends there.
+ */
+
+
+#include "pch.h"
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!\n"; 
+}
